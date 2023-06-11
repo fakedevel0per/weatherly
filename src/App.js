9fe86e6a2c25814a1b09f07a4011b53c4ee9772c
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-06-11 14:57:52 +0530

    get data by ip

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ import Content from './components/Content';
 import Location from './components/Location';
 
 function App() {
-  // const apikey = process.env.REACT_APP_IPDATA_API_KEY;
-  
+  const ipDataApiKey = process.env.REACT_APP_IPDATA_API_KEY;
+  const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
   // //DOESNT WORK WITH LOCALHOST
   // // async function getIP(){
   // //   let data = await fetch("https://checkip.amazonaws.com/").then(res=>res.text()).then(data=>console.log(data))
   // // }
   // // getIP();
   // console.log(apikey);
+
   const [current, setCurrent] = useState({})
   const [inputLocation, setInputLocation] = useState(null)
+  const [ipAddress, setIpAddress] = useState(null)
 
-  // const IPData = require('ipdata').default;
-  // var userIP;
-  // const cacheConfig = {
-  //   max: 1000, // max size
-  //   maxAge: 10 * 60 * 1000, // max age in ms (i.e. 10 minutes)
-  // };
-  // const ipdata = new IPData('ef71ac7d6f1983797c444931ab9246c7b86eecf134c15b0ed93bc8c6', cacheConfig);
-  // ipdata.lookup()
-  // .then(function(info) {
-  //   userIP = info.ip;
-  // });
-                     //GET DATA
+  const IPData = require('ipdata').default;
+
+  const getIP = async()=>{
+    const cacheConfig = {
+      max: 1000, // max size
+      maxAge: 10 * 60 * 1000, // max age in ms (i.e. 10 minutes)
+    };
+    const ipdata = new IPData(ipDataApiKey, cacheConfig);
+    const res = await ipdata.lookup().then(info=>setIpAddress(info.ip));
+  }
+                    //GET DATA
         const getCurrentData = async() =>{
-          const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=d4171a20a12f4b98a82171452230906&q=mumbai`).then(res=> res.json()).then(data=> setCurrent(data)).catch(err=>console.log(err));
+        const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${ipAddress}`).then(res=> res.json()).then(data=> setCurrent(data)).catch(err=>console.log(err));
         console.log(current.location.name)
         }
 
         useEffect(() => {
+          getIP();
           getCurrentData();
-        }, [inputLocation])
+        }, [])
         
   return (
     <div className="App">
--- src/components/Location.js
+++ src/components/Location.js
@@ -2,8 +2,8 @@ import React from 'react'
 const Location = (props) => {
     const {location} = props
   return (
-    <div className="mt-3 me-3 ms-3 p-2 w-80 border border-primary rounded">
-      Name : {location.name} Region : {location.region} Country : {location.country} Time-Zone = {location.tz_id} Local Time : {location.localtime} ({location.lat}, {location.lon})
+    <div className="mt-3 me-3 ms-3 p-2 w-80 border border-primary-suble text-primary-emphasis bg-primary-subtle rounded d-flex flex-wrap justify-content-between">
+      {/* <div>Name : {location.name}</div><div>Region : {location.region}</div><div>Country : {location.country}</div><div>Time-Zone = {location.tz_id}</div><div>Local Time : {location.localtime}</div><div>({location.lat}<span>&#176;</span>, {location.lon}<span>&#176;</span>)</div> */}
     </div>
   )
 }
